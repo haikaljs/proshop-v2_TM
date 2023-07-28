d3a3176eef3318933d63363d8dab81ef3811fee9
haikaljs <haikaljavascript@gmail.com>
2023-07-29 02:43:44 +0800

    Create review schema

--- models/productModel.js
+++ models/productModel.js
@@ -1,5 +1,27 @@
 import mongoose from "mongoose";
 
+const reviewSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
 const productSchema = new mongoose.Schema(
   {
     user: {
