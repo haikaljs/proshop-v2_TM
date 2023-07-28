0200e31a29dd4517d5117a5451c860b4b7ff7396
haikaljs <haikaljavascript@gmail.com>
2023-07-29 02:06:17 +0800

    Fix proxy error and mongodb ip connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
+
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 dotenv.config();
 import products from "./data/products.js";
 
 const port = process.env.PORT || 5000;
+
+connectDB(); // Connect to mongodb
 
 const app = express();
 
