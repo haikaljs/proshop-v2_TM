b5786e65adc42cbb360755deb7d626b00ead3019
haikaljs <haikaljavascript@gmail.com>
2023-07-29 05:15:12 +0800

    Create routes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 dotenv.config();
-import products from "./data/products.js";
+
+import productRoutes from "./routes/productRoutes.js";
 
 const port = process.env.PORT || 5000;
 
@@ -11,18 +12,7 @@ connectDB(); // Connect to mongodb
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API is running using...");
-});
-
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
